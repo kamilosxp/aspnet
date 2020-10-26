02e8578ce45b296326744485bcc00c8062c50b43
Kamil Rudek <rudekkamil@gmail.com>
2020-10-26 12:32:36 +0100

    update repo to actual status

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetProject.Controllers
+{
+    [ApiController]
+    [Route("api/AjaxController")]
+    public class AjaxController : ControllerBase
+    {
+
+        public IActionResult Post(CompanyModel model)
+        {
+
+            return null;
+        }
+    }
+}
---
+++ Models/AddNewItemConfirmationViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetProject
+{
+    public class AddNewItemConfirmationViewModel
+    {
+        public int Id { get; internal set; }
+
+        public string Name { get; internal set; }
+    }
+}
