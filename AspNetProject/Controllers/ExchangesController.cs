0080360482ebb89b5c67f04ed91f6cde5045ca8d
Kamil Rudek <rudekkamil@gmail.com>
2020-10-27 20:44:45 +0100

    remove second repository file

---
+++ AspNetProject/Controllers/ExchangesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetProject.Filters;
+using AspNetProject.Models;
+
+namespace AspNetProject
+{
+    public class ExchangesController : Controller
+    {
+        [ServiceFilter(typeof(MyCustomActionFilter))]
+        public IActionResult Show(string id)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(ItemModel item)
+        {
+            // TODO add to database
+
+            var viewModel = new AddNewItemConfirmationViewModel
+            {
+                Id = 1,
+                Name = item.Name,
+            };
+
+            return View("AddConfirmation", viewModel);
+        }
+    }
+}
---
+++ AspNetProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
