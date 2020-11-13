c6f00bfee271c57c2bf8f63aba054e6fdfad2a48
Kamil Rudek <rudekkamil@gmail.com>
2020-11-13 18:18:03 +0100

    implement ajax

---
+++ AspNetProject/Controllers/ExchangesSecondController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetProject.Filters;
+using AspNetProject.Models;
+
+namespace AspNetProject
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangesSecondController : ControllerBase
+    {
+        [HttpPost]
+        [Route("AddNewItem")]
+        public IActionResult Post(ItemModel model)
+        {
+            return Ok();
+        }
+    }
+}
--- AspNetProject/wwwroot/js/addNewItem.js
+++ AspNetProject/wwwroot/js/addNewItem.js
@@ -1,27 +1,34 @@
-﻿(function () {
+﻿document.addEventListener("DOMContentLoaded", () => {
     const alertElement = document.getElementById("success-alert");
     const formElement = document.forms[0];
-    const addNewItem = async () => {
-        let formData = {
-            Name: "aThing",
-            Description: "FancyThing Here ",
-            IsVisible: true
+    formElement.addEventListener("submit", (event) => addnewItem(event))
+
+    const addnewItem = async (e) => {
+        e.preventDefault();
+        const nameValue = document.getElementById("Name").value;
+        const descriptionValue = document.getElementById("Description").value;
+        const isVisibleValue = document.getElementById("IsVisible").checked;
+
+        const companyModel = {
+            Name: nameValue,
+            Description: descriptionValue,
+            IsVisible: isVisibleValue
         };
 
-        const response = await fetch("/api/Listing", {
-            method: 'POST',
-            headers: { "Content-type": "application/json" },
-            body: JSON.stringify(formData)
-        })
+        const response = await fetch("/api/ExchangesSecond/AddNewItem", {
+            method: "POST",
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(companyModel)
+        });
 
         if (response.ok) {
             alertElement.style.display = "block";
+            setTimeout(() => {
+                alertElement.style.display = "none";
+            }, 2000);
+
         }
-    };
-    window.addEventListener("load", () => {
-        formElement.addEventListener("submit", event => {
-            event.preventDefault();
-            addNewItem().then(() => console.log("added successfully"));
-        });
-    });
-})();
+    }
+})
