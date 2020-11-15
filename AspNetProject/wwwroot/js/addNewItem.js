183f2c32c7ebad92d57d5d2d1b36bd5ab8ac8afc
Kamil Rudek <rudekkamil@gmail.com>
2020-11-15 10:58:01 +0100

    fix ajax implementation

--- AspNetProject/wwwroot/js/addNewItem.js
+++ AspNetProject/wwwroot/js/addNewItem.js
@@ -1,10 +1,8 @@
-﻿document.addEventListener("DOMContentLoaded", () => {
+﻿(function () {
     const alertElement = document.getElementById("success-alert");
     const formElement = document.forms[0];
-    formElement.addEventListener("submit", (event) => addnewItem(event))
+    const addNewItem = async () => {
 
-    const addnewItem = async (e) => {
-        e.preventDefault();
         const nameValue = document.getElementById("Name").value;
         const descriptionValue = document.getElementById("Description").value;
         const isVisibleValue = document.getElementById("IsVisible").checked;
@@ -16,19 +14,24 @@
         };
 
         const response = await fetch("/api/ExchangesSecond/AddNewItem", {
-            method: "POST",
-            headers: {
-                'Content-Type': "application/json"
-            },
+            method: 'POST',
+            headers: { "Content-type": "application/json" },
             body: JSON.stringify(companyModel)
-        });
+        })
 
         if (response.ok) {
             alertElement.style.display = "block";
             setTimeout(() => {
-                alertElement.style.display = "none";
-            }, 2000);
+                    alertElement.style.display = "none";
+                },
+                2000);
 
         }
-    }
-})
+    };
+    window.addEventListener("load", () => {
+        formElement.addEventListener("submit", event => {
+            event.preventDefault();
+            addNewItem().then(() => console.log("added successfully"));
+        });
+    });
+})();
